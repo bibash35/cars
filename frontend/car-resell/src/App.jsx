925b5f1f4027b98779c4a90f54781d962bd8335c
Bibash23 <162779367+Bibash23@users.noreply.github.com>
2025-01-28 21:00:55 +0545

    first commit

---
+++ frontend/car-resell/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Signup from "./Components/Signup"
+import Login from "./Components/Login";
+
+function App() {
+
+  return (
+    <>
+     <Signup />
+     {/* <Login/> */}
+
+    </>
+  )
+}
+
+export default App
